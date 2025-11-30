889c305ca5b6e51bec1b1bc66efb6c5fea19d4b9
Ian Alvarez <ianalvarez31@gmail.com>
2025-11-30 07:37:29 -0500

    BE Ventanilla ECUSOL

---
+++ src/main/java/com/ecusol/ventanilla/EcusolCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecusol.ventanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcusolCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcusolCoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecusol/ventanilla/controller/AuthController.java
@@ -0,0 +1,22 @@
+//ubi: src/main/java/com/ecusol/ventanilla/controller/AuthController.java
+package com.ecusol.ventanilla.controller;
+
+import com.ecusol.ventanilla.dto.JwtResponse;
+import com.ecusol.ventanilla.dto.LoginRequest;
+import com.ecusol.ventanilla.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+}
